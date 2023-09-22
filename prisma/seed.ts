c2987179bff73b9d2112f62070cbfc4b946ac3e7
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-09-22 16:23:14 +0200

    feat: prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@prisma/client';
+    
+const prisma = new PrismaClient();
+    
+async function main() {
+    await prisma.snippet.create({
+        data: {
+            title: 'Hello World',
+            code:
+`main()
+{
+    printf("hello, world\\n");
+}`,
+            description: 'Code original publié dans "The C Programming Language" de Brian Kernighan et Dennis Ritchie.',
+            creationDate: new Date(2023, 4, 8, 9, 12, 36),
+        }
+    });
+    await prisma.snippet.create({
+        data: {
+            title: 'Il faut protéger ses chaînes de caractères',
+            code: '<script>window.alert("Injection !")</script>',
+            creationDate: new Date(2023, 3, 4, 5, 6, 7),
+            description: 'Dans le template EJS, observez le comportement de la page en utilisant successivement les balises <%- et <%=pour injecter les données.', 
+        }
+    });
+    await prisma.snippet.create({
+        data: {
+            title: 'Il faut télécharger les scripts skyler',
+            code: ' <a href="/images/myw3schoolsimage.jpg" download>',
+            creationDate: new Date(2023, 3, 4, 5, 6, 7),
+            description: 'Cela permet de télcharger un fichier avec son lien en cliquant dessus', 
+        }
+    });
+}
+
+main().catch(e => {
+    console.error(e);
+    process.exit(1);
+}).finally(async () => {
+    await prisma.$disconnect();
+});
