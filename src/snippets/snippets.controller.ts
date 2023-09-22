44ec2543307e38681c85874eb4720c946a3191f4
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-09-22 15:32:47 +0200

    feat: async handler

--- src/snippets/snippets.controller.ts
+++ src/snippets/snippets.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export class SnippetsController {
-    static list(req: Request, res: Response, next: NextFunction): void {
+    static async list(req: Request, res: Response, next: NextFunction) {
         throw new Error("Ceci est un message d'erreur");
     }
 }
--- src/snippets/snippets.routes.ts
+++ src/snippets/snippets.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { SnippetsController } from "./snippets.controller";
+import expressAsyncHandler from 'express-async-handler'
 
 const router = express.Router();
 
-router.get('/', SnippetsController.list)
+router.get('/', expressAsyncHandler(SnippetsController.list))
 
 export default router;
