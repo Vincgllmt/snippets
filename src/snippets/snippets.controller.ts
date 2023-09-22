5dbcf11cdffe775ab6e0f2f4e2b1468d5e3d47fd
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-09-22 17:24:37 +0200

    wip: language filter

--- src/snippets/snippets.controller.ts
+++ src/snippets/snippets.controller.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../services/prisma";
+import { validationResult } from "express-validator";
 
 export class SnippetsController {
     static async list(req: Request, res: Response, next: NextFunction) {
-        const snippets = await prisma.snippet.findMany({
-            include: {
-                language: true,
-            }
-        });
-        res.render('snippets/snippets_list', { snippets })
+        if(validationResult(req).isEmpty()){
+            const snippets = await prisma.snippet.findMany({
+                include: {
+                    language: true,
+                },
+                where: {
+                    languageId: req.params.langId ? +req.params.langId : undefined
+                },
+            });
+            res.render('snippets/snippets_list', { snippets })
+        }else{
+            throw new Error('Invalid param');
+        }
     }
 }
--- src/snippets/snippets.routes.ts
+++ src/snippets/snippets.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { SnippetsController } from "./snippets.controller";
 import expressAsyncHandler from 'express-async-handler'
+import { param } from "express-validator";
 
 const router = express.Router();
 
-router.get('/', expressAsyncHandler(SnippetsController.list))
+router.get('/:langId?', param("langId").optional().isNumeric(),expressAsyncHandler(SnippetsController.list))
 
 export default router;
